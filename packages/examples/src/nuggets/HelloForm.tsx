23c2bb615cfc3d6e5fd59693018b3cf668ba2c92
Jack Scott <mast.j.scott@gmail.com>
2019-01-12 17:07:54 +0800

    added some more random styles

--- packages/examples/src/nuggets/HelloForm.tsx
+++ packages/examples/src/nuggets/HelloForm.tsx
@@ -1,11 +1,16 @@
 import React, { FunctionComponent } from 'react';
-import { Form, Input } from 'nuggets';
+import { Square, Text } from 'nuggets';
+
+const niceShadow = [
+  { down: -100, color: 'blue', blur: 20, grow: 10 },
+  { down: -100, color: 'rgba(0, 0, 0, 0.3)', blur: 20, grow: 30 },
+  { down: -100, color: 'yellow', blur: 20, grow: 50 },
+];
 
 const HelloForm: FunctionComponent = () => (
-  <Form>
-    <Input name="name" />
-    <button type="button">Submit</button>
-  </Form>
+  <Square color="green" shadow={niceShadow}>
+    <Text color="yellow">Dan Scott</Text>
+  </Square>
 );
 
 export default HelloForm;
