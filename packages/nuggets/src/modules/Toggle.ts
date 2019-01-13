af0b46b7e17231e1a79bdb0d2d8b7453746f0b5f
Jack Scott <mast.j.scott@gmail.com>
2019-01-13 18:58:43 +0800

    created the toggle class and other empty comps

---
+++ packages/nuggets/src/modules/Toggle.ts
@@ -0,0 +1,48 @@
+import {
+  FunctionComponent,
+  ReactNode,
+  useState,
+  useEffect,
+  ReactElement,
+} from 'react';
+
+export interface IToggleChildren {
+  on: (...args: any[]) => any;
+  off: (...args: any[]) => any;
+  toggle: (override?: boolean, ...args: any[]) => any;
+  active: boolean;
+}
+
+export interface IToggleProps {
+  value?: any;
+  change?: (value: any) => any;
+  children: ({ on, off, toggle, active }: IToggleChildren) => ReactNode;
+}
+
+export const Toggle: FunctionComponent<IToggleProps> = ({
+  children,
+  ...options
+}) => {
+  const [value, change] = useState<boolean>(options.value);
+  useEffect(
+    () => {
+      change(options.value);
+    },
+    [options.value]
+  );
+  const update = (next: boolean) => {
+    change(next);
+    if (options.change) {
+      options.change(next);
+    }
+  };
+  return children({
+    on: () => update(true),
+    off: () => update(false),
+    toggle: override =>
+      update(typeof override === 'boolean' ? override : !value),
+    active: value,
+  }) as ReactElement<any>;
+};
+
+Toggle.displayName = 'Toggle';
