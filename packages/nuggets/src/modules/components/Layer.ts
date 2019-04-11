8ed005b0df0d8ec47b9c37f27841f197ce857c46
Jack Scott <jack.rob.scott@gmail.com>
2019-04-11 16:55:02 +0800

    added absolute option to layer

--- packages/nuggets/src/modules/components/Layer.ts
+++ packages/nuggets/src/modules/components/Layer.ts
@@ -7,19 +7,21 @@ export type ILayerProps = INuggieProps<{}> & {
   children?: INonTextChildren;
   node?: HTMLElement | null;
   id?: string;
+  absolute?: boolean;
 };
 
 export const Layer: FunctionComponent<ILayerProps> = ({
   children,
   node,
   id,
+  absolute,
   ...options
 }) => {
   const item = node || document.getElementById(id || 'root');
   const precss = {
     display: 'flex',
-    overflow: 'hidden',
-    position: 'fixed',
+    overflow: absolute ? 'visible' : 'hidden',
+    position: absolute ? 'absolute' : 'fixed',
     pointerEvents: 'none',
     top: 0,
     left: 0,
