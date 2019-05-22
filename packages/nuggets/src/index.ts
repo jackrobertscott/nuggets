d57aa38845059d0c096dd91700918a02c3584820
Jack Scott <jack.rob.scott@gmail.com>
2019-05-22 14:22:41 +0800

    change complex to schema

--- packages/nuggets/src/index.ts
+++ packages/nuggets/src/index.ts
@@ -16,11 +16,11 @@ export * from './modules/helpers/createStore';
  */
 export * from './modules/hooks/useAddress';
 export * from './modules/hooks/useArray';
-export * from './modules/hooks/useComplex';
 export * from './modules/hooks/useConnection';
 export * from './modules/hooks/useDatetime';
 export * from './modules/hooks/useMedia';
 export * from './modules/hooks/useNumber';
+export * from './modules/hooks/useSchema';
 export * from './modules/hooks/useStore';
 export * from './modules/hooks/useString';
 export * from './modules/hooks/useStyles';
---
+++ packages/nuggets/src/modules/hooks/useSchema.ts
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+
+export interface ISchemaValue {
+  [name: string]: any;
+}
+
+export interface ISchemaDirty {
+  [name: string]: boolean;
+}
+
+export interface ISchemaValidate {
+  [name: string]: (data: any) => Promise<any> | any;
+}
+
+export interface ISchemaError {
+  [name: string]: Error;
+}
+
+export interface IuseSchemaOptions {
+  initial: ISchemaValue;
+  schema: ISchemaValidate;
+  change?: (value: ISchemaValue) => any;
+}
+
+export interface IuseSchemaProps {
+  fields: {
+    [name: string]: {
+      value: any;
+      error?: Error;
+      dirty: boolean;
+      change: (data: any) => void;
+      touch: (status?: boolean) => void;
+    };
+  };
+  value: ISchemaValue;
+  error: ISchemaError;
+  dirty: ISchemaDirty;
+  invalid: boolean;
+  change: (value: ISchemaValue) => any;
+  override: (value?: ISchemaValue) => void;
+  touch: (override?: boolean) => void;
+}
+
+export const useSchema = ({
+  initial,
+  change,
+  schema,
+}: IuseSchemaOptions): IuseSchemaProps => {
+  const [state, update] = useState<ISchemaValue>(initial || {});
+  const [error, updateError] = useState<ISchemaError>({});
+  const [dirty, updateDirty] = useState<ISchemaDirty>({});
+  const [globalDirty, updateGlobalDirty] = useState<boolean>(false);
+  const override = (next?: ISchemaValue) => {
+    const data = next || {};
+    update(data);
+    if (change) {
+      change(data);
+    }
+    if (schema) {
+      const tasks = Object.keys(schema).map(key => {
+        try {
+          const status = schema && schema[key](data[key]);
+          if (status instanceof Promise) {
+            return status
+              .then(() => ({ [key]: undefined }))
+              .catch((e: Error) => ({ [key]: e }));
+          }
+        } catch (e) {
+          return Promise.resolve({ [key]: e });
+        }
+        return Promise.resolve({ [key]: undefined });
+      });
+      Promise.all(tasks as Array<Promise<any>>).then(values => {
+        const issues = values.reduce((all, e) => {
+          return { ...all, ...(e || {}) };
+        }, {});
+        updateError(issues);
+      });
+    }
+  };
+  const mutate = (next?: ISchemaValue) => {
+    override({ ...state, ...(next || {}) });
+  };
+  const blur = (name: string, status?: boolean) => {
+    if (name) {
+      updateDirty({ ...dirty, [name]: status || true });
+    }
+  };
+  const reduce = (all: any, key: string) => ({
+    ...all,
+    [key]: {
+      value: state[key],
+      error: error[key],
+      dirty: globalDirty || dirty[key] || !!state[key],
+      change: (data: any) => mutate({ [key]: data }),
+      touch: (status?: boolean) => blur(key, status),
+    },
+  });
+  return {
+    fields: Object.keys(schema).reduce(reduce, {}),
+    value: state,
+    dirty,
+    error,
+    invalid: !!Object.keys(error).filter(key => error[key]).length,
+    change: mutate,
+    override,
+    touch: (status?: boolean) => updateGlobalDirty(status || true),
+  };
+};
